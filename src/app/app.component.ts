62bd04af7582bb1ee88853597619d574dee5fbb4
Gungun <gungun@Gunguns-MacBook-Pro.local>
2018-09-23 09:18:14 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  //declaring variable for demoing pipis
+  name="abhishek"
+  //creating object for demoing json pipe
+  public person ={
+    "firstname":"abhishek",
+    "lastname":"Ghosh"
+  }
+
+}
